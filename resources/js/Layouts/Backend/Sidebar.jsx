ca5c5f2aa823b61969cdf24fba0bcfb9fa12eb99
MH Munna <monirhasanmunna1@gmail.com>
2025-10-05 17:42:21 +0600

    [Add] sidebar arrange

--- resources/js/Layouts/Backend/Sidebar.jsx
+++ resources/js/Layouts/Backend/Sidebar.jsx
@@ -1,7 +1,12 @@
 
 import {Link, usePage} from "@inertiajs/react";
-import {IoHomeSharp} from "react-icons/io5";
 import {FaGear} from "react-icons/fa6";
+import {BsInfoSquare} from "react-icons/bs";
+import {RiPagesLine} from "react-icons/ri";
+import {AiOutlineProduct} from "react-icons/ai";
+import {FaRegNewspaper} from "react-icons/fa";
+import {LiaUsersSolid} from "react-icons/lia";
+import {LuGalleryVertical} from "react-icons/lu";
 
 export default function Sidebar() {
     const { url: currentUrl } = usePage()
@@ -19,7 +24,7 @@ export default function Sidebar() {
                 <nav className="hs-accordion-group p-3 w-full flex flex-col flex-wrap" data-hs-accordion-always-open="true">
                     <ul className="flex flex-col space-y-1">
                         <li>
-                            <Link href={route('dashboard')} className="flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-[15px] text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-white">
+                            <Link href={route('dashboard')} className={`flex items-center ${route().current('dashboard') ? 'bg-gray-200' : ''} gap-x-3.5 py-2 px-2.5 bg-gray-100 text-[15px] text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-white`}>
                                 <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
                                 Dashboard
                             </Link>
@@ -27,91 +32,45 @@ export default function Sidebar() {
 
                         <li>
                             <Link href={route('admin.hero_section.page')} className={`${route().current('admin.hero_section.page') ? 'bg-gray-200' : ''} flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-[15px] text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-white`}>
-                                <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
+                                <LuGalleryVertical className={`size-4.5`} />
                                 Hero Section
                             </Link>
                         </li>
 
                         <li>
                             <Link href={route('admin.about_section.page')} className={`${route().current('admin.about_section.page') ? 'bg-gray-200' : ''} flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-[15px] text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-white`}>
-                                <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
+                                <BsInfoSquare className={`size-4.5`} />
                                 About Section
                             </Link>
                         </li>
 
                         <li>
                             <Link href={route('admin.page.list')} className={`${route().current('admin.page.list') ? 'bg-gray-200' : ''} flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-[15px] text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-white`}>
-                                <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
+                                <RiPagesLine  className={`size-4.5`} />
                                 Page
                             </Link>
                         </li>
 
-                        <li className={`hs-accordion ${route().current('admin.product.*') ? 'active' : ''}`} id="account-accordion">
-                            <button type="button" className={`hs-accordion-toggle ${route().current('admin.product.*') ? 'bg-gray-200 ' : ''}w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-[15px] text-gray-800 rounded-lg hs-accordion-active:rounded-b-none hover:bg-gray-100 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-neutral-200" aria-expanded="true" aria-controls="account-accordion-child`}>
-                                <IoHomeSharp className={`size-4.5`} />
+                        <li>
+                            <Link href={route('admin.product.list')} className={`${route().current('admin.product.list') ? 'bg-gray-200' : ''} flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-[15px] text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-white`}>
+                                <AiOutlineProduct  className={`size-4.5`} />
                                 Product
-
-                                <svg className="hs-accordion-active:block ms-auto hidden size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-
-                                <svg className="hs-accordion-active:hidden ms-auto block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m6 9 6 6 6-6"/></svg>
-                            </button>
-
-                            <div id="account-accordion-child" className={`${route().current('admin.product.*') ? 'block' : 'hidden'} hs-accordion-content bg-gray-200 w-full overflow-hidden transition-[height] duration-300 hidden" role="region" aria-labelledby="account-accordion`}>
-                                <ul className="p-1.5 space-y-1">
-                                    <li>
-                                        <Link href={route('admin.product.list')} className={`flex items-center gap-x-2.5 pr-2.5 py-1 text-[15px] text-gray-800 rounded-lg ${route().current('admin.product.list') ? 'bg-gray-50' : ''} hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-neutral-200`}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 15a3 3 0 1 1 0-6a3 3 0 0 1 0 6Z"/></svg>
-                                            Product List
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
+                            </Link>
                         </li>
 
-                        <li className={`hs-accordion ${route().current('admin.warranty.*') ? 'active' : ''}`} id="account-accordion">
-                            <button type="button" className={`hs-accordion-toggle ${route().current('admin.warranty.*') ? 'bg-gray-200 ' : ''}w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-[15px] text-gray-800 rounded-lg hs-accordion-active:rounded-b-none hover:bg-gray-100 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-neutral-200" aria-expanded="true" aria-controls="account-accordion-child`}>
-                                <IoHomeSharp className={`size-4.5`} />
+                        <li>
+                            <Link href={route('admin.warranty.list')} className={`${route().current('admin.warranty.list') ? 'bg-gray-200' : ''} flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-[15px] text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-white`}>
+                                <FaRegNewspaper  className={`size-4.5`} />
                                 Warranty
-
-                                <svg className="hs-accordion-active:block ms-auto hidden size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-
-                                <svg className="hs-accordion-active:hidden ms-auto block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m6 9 6 6 6-6"/></svg>
-                            </button>
-
-                            <div id="account-accordion-child" className={`${route().current('admin.warranty.*') ? 'block' : 'hidden'} hs-accordion-content bg-gray-200 w-full overflow-hidden transition-[height] duration-300 hidden" role="region" aria-labelledby="account-accordion`}>
-                                <ul className="p-1.5 space-y-1">
-                                    <li>
-                                        <Link href={route('admin.warranty.list')} className={`flex items-center gap-x-2.5 pr-2.5 py-1 text-[15px] text-gray-800 rounded-lg ${route().current('admin.warranty.list') ? 'bg-gray-50' : ''} hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-neutral-200`}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 15a3 3 0 1 1 0-6a3 3 0 0 1 0 6Z"/></svg>
-                                            Warranty List
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
+                            </Link>
                         </li>
 
-                        <li className={`hs-accordion ${route().current('admin.distributor.*') ? 'active' : ''}`} id="account-accordion">
-                            <button type="button" className={`hs-accordion-toggle ${route().current('admin.distributor.*') ? 'bg-gray-200 ' : ''}w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-[15px] text-gray-800 rounded-lg hs-accordion-active:rounded-b-none hover:bg-gray-100 focus:outline-hidden focus:bg-gray-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-neutral-200" aria-expanded="true" aria-controls="account-accordion-child`}>
-                                <IoHomeSharp className={`size-4.5`} />
+                        <li>
+                            <Link href={route('admin.distributor.list')} className={`${route().current('admin.distributor.list') ? 'bg-gray-200' : ''} flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-[15px] text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-white`}>
+                                <LiaUsersSolid  className={`size-4.5`} />
                                 Distributor
-
-                                <svg className="hs-accordion-active:block ms-auto hidden size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m18 15-6-6-6 6"/></svg>
-
-                                <svg className="hs-accordion-active:hidden ms-auto block size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m6 9 6 6 6-6"/></svg>
-                            </button>
-
-                            <div id="account-accordion-child" className={`${route().current('admin.distributor.*') ? 'block' : 'hidden'} hs-accordion-content bg-gray-200 w-full overflow-hidden transition-[height] duration-300 hidden" role="region" aria-labelledby="account-accordion`}>
-                                <ul className="p-1.5 space-y-1">
-                                    <li>
-                                        <Link href={route('admin.distributor.list')} className={`flex items-center gap-x-2.5 pr-2.5 py-1 text-[15px] text-gray-800 rounded-lg ${route().current('admin.distributor.list') ? 'bg-gray-50' : ''} hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-neutral-200`}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 15a3 3 0 1 1 0-6a3 3 0 0 1 0 6Z"/></svg>
-                                            Distributor List
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
+                            </Link>
                         </li>
-
                     </ul>
                 </nav>
 
